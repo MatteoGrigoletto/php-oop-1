c82b9f7347324b9df3b596bdc083ed0e9618ab7b
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-09 16:37:53 +0100

    Aggiunge stile

--- index.php
+++ index.php
@@ -9,20 +9,24 @@ require  __DIR__ . '/classes/movieClass.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>List Films</title>
 </head>
 <body>
-    <?php
+    <div class="container">
+        <h1>Lista film</h1>
+            <?php
 
-  for($i = 0; $i < count($films) ; $i++) {
-    $film= new movie($films[$i]['name'],$films[$i]['duration'],$films[$i]['adult'],$films[$i]['publication']);
- ?>
- <ul>
-    <li><?php echo $film->getTitle() ?></li>
-    <li><?php echo $film->durate ?></li>
-    <li><?php echo $film->yearPublication ?></li>
-    <li><?php echo $film->movieYears?></li>
- </ul>
- <?php }; ?>
+        for($i = 0; $i < count($films) ; $i++) {
+            $film= new movie($films[$i]['name'],$films[$i]['duration'],$films[$i]['adult'],$films[$i]['publication']);
+        ?>
+        <ul>
+            <li><?php echo $film->getTitle() ?></li>
+            <li><?php echo $film->durate ?></li>
+            <li><?php echo $film->yearPublication ?></li>
+            <li><?php echo $film->movieYears?></li>
+        </ul>
+        <?php }; ?>
+    </div>
 </body>
 </html>
